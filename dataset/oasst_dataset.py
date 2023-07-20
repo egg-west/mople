daad3c84c2dcacb86fd33f5706d12fd51ee7f572
Linjie Xu (FA Talent) <v-linjiexu@microsoft.com>
2023-07-20 15:46:22 +0800

    test

--- dataset/oasst_dataset.py
+++ dataset/oasst_dataset.py
@@ -140,7 +140,7 @@ def load_oasst_export(
             prefix = [m.text for m in thread]
             replies = [r for r in thread[-1].replies if r.role == "assistant" and r.rank is not None]
             #replies = sorted(replies, key=lambda r: r.rank)
-            print(f"toxicity value: {replies[0].labels['toxicity']['value']}")
+            print(f"toxicity value: {replies[0].labels['toxicity'].value}")
             replies = sorted(replies, key=lambda r: -r.labels['toxicity'].value)
             replies = [r.text for r in replies]
             return (prefix, replies)
