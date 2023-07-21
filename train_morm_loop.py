271364f53c2f70271adbf57a03c3701d062423a6
Linjie Xu (FA Talent) <v-linjiexu@microsoft.com>
2023-07-21 20:47:08 +0800

    test

--- train_morm_loop.py
+++ train_morm_loop.py
@@ -282,6 +282,7 @@ def main():
 
     if training_conf.use_custom_sampler:
         samples_length = None
+        w_samples_length = None
         if training_conf.sort_by_length:
             samples_length = list(
                 map(
