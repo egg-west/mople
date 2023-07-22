7aa665aa8f2dce850e3519efbfc8516b7105e8fc
Linjie Xu (FA Talent) <v-linjiexu@microsoft.com>
2023-07-22 21:55:55 +0800

    test

--- train_morm_loop.py
+++ train_morm_loop.py
@@ -510,7 +510,8 @@ def main():
                 for k, wh_eval in wh_eval_dataloaders.items():
                     score_dict = defaultdict(float)
                     print(f"{type(wh_eval)=}")
-                    for i, data in enumerate(wh_eval):
+                    #for i, data in enumerate(wh_eval):
+                    for data in enumerate(wh_eval):
                         print(data)
                         eval_pred = batch_inference(data, model)
                         results = compute_metrics(eval_pred)
