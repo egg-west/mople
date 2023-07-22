589eafc899168dfd962435c919db733178b900fa
Linjie Xu (FA Talent) <v-linjiexu@microsoft.com>
2023-07-22 13:53:24 +0800

    test

--- train_morm_loop.py
+++ train_morm_loop.py
@@ -436,12 +436,12 @@ def main():
 
             # train with data of [0,...,1,...,0] preference
             default_batch_tuple = next(enumerate(w_train_dataloader))[1]
-            print(f"{len(default_batch_tuple)=}")
-            print(f"{default_batch_tuple[0].keys()=}")
+            # print(f"{len(default_batch_tuple)=}") # 3
+            # print(f"{default_batch_tuple[0].keys()=}")
 
             batch = {k: v.to(device) for k, v in default_batch_tuple[0].items()}
 
-            batch_tuple = (batch, default_batch_tuple[1])
+            batch_tuple = (batch, default_batch_tuple[1], default_batch_tuple[2])
             loss, outputs = trainer.compute_w_loss(model, batch_tuple, return_logits=True)
 
             loss.backward()
