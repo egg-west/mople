8657c9377bb739c87e987ff84fee5c3352b08dab
Linjie Xu (FA Talent) <v-linjiexu@microsoft.com>
2023-07-25 20:45:54 +0800

    test

--- train_helpful.py
+++ train_helpful.py
@@ -321,7 +321,10 @@ def argument_parsing(notebook=False, notebook_args=None):
     return parser.parse_args(remaining)
 
 training_conf = argument_parsing()
-ds_config = DeepSpeedConfig(training_conf.deepspeed_config)
+import json
+f = open(training_conf.deepspeed_config)
+ds_config = json.load(f)
+#ds_config = DeepSpeedConfig(training_conf.deepspeed_config)
 monitor = MonitorMaster(ds_config.monitor_config)
 
 def main():
