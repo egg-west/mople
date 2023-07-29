cd7ac251a63c256272d074b0946ed6c867f49571
Linjie Xu (FA Talent) <v-linjiexu@microsoft.com>
2023-07-29 15:23:01 +0800

    add task tag in the text

--- dataset/hh_dataset.py
+++ dataset/hh_dataset.py
@@ -135,10 +135,14 @@ class AnthropicRLHFH(Dataset):
                 if n_obj == None:
                     self.data.append((prefix, [good_reply, bad_reply]))
                 else:
+                    if "helpful" in objective:
+                        suffix = " helpful"
+                    else:
+                        suffix = " harmless"
                     preference = np.zeros((2, n_obj))
                     preference[0, obj_id] = 1.0
                     preference[1, obj_id] = 1.0
-                    self.data.append((prefix, [good_reply, bad_reply], preference))
+                    self.data.append((prefix, [good_reply + suffix, bad_reply + suffix], preference))
 
     def __len__(self) -> int:
         return len(self.data)
